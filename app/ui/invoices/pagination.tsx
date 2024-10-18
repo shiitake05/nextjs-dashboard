572642a99c60967e61a90d3ed7111512124de8c1
shiitake <pegtar377512@gmail.com>
2024-10-19 07:11:51 +0900

    fix: version error

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -3,7 +3,6 @@
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
@@ -62,36 +61,36 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   );
 }
 
-function PaginationNumber({
-  page,
-  href,
-  isActive,
-  position,
-}: {
-  page: number | string;
-  href: string;
-  position?: 'first' | 'last' | 'middle' | 'single';
-  isActive: boolean;
-}) {
-  const className = clsx(
-    'flex h-10 w-10 items-center justify-center text-sm border',
-    {
-      'rounded-l-md': position === 'first' || position === 'single',
-      'rounded-r-md': position === 'last' || position === 'single',
-      'z-10 bg-blue-600 border-blue-600 text-white': isActive,
-      'hover:bg-gray-100': !isActive && position !== 'middle',
-      'text-gray-300': position === 'middle',
-    },
-  );
+// function PaginationNumber({
+//   page,
+//   href,
+//   isActive,
+//   position,
+// }: {
+//   page: number | string;
+//   href: string;
+//   position?: 'first' | 'last' | 'middle' | 'single';
+//   isActive: boolean;
+// }) {
+//   const className = clsx(
+//     'flex h-10 w-10 items-center justify-center text-sm border',
+//     {
+//       'rounded-l-md': position === 'first' || position === 'single',
+//       'rounded-r-md': position === 'last' || position === 'single',
+//       'z-10 bg-blue-600 border-blue-600 text-white': isActive,
+//       'hover:bg-gray-100': !isActive && position !== 'middle',
+//       'text-gray-300': position === 'middle',
+//     },
+//   );
 
-  return isActive || position === 'middle' ? (
-    <div className={className}>{page}</div>
-  ) : (
-    <Link href={href} className={className}>
-      {page}
-    </Link>
-  );
-}
+//   return isActive || position === 'middle' ? (
+//     <div className={className}>{page}</div>
+//   ) : (
+//     <Link href={href} className={className}>
+//       {page}
+//     </Link>
+//   );
+// }
 
 function PaginationArrow({
   href,
