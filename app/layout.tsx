0594825a9340bb230a4f4f0ee1486dba9b4d5b29
shiitake <pegtar377512@gmail.com>
2024-10-19 05:59:59 +0900

    feat: add application system

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,19 @@
+<<<<<<< Updated upstream
+=======
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
+>>>>>>> Stashed changes
 export default function RootLayout({
   children,
 }: {
